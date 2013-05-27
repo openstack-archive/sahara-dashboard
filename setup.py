dc4b178753f15faf310071a08df8389e1ab8a4d7
Nikita Konovalov <nkonovalov@mirantis.com>
2013-05-27 12:07:53 +0400

    Initial commit for savanna dashboard.
    Change-Id: I824bee3aa7e86e9208e17d9c913f8ae7b7a97cd5

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+from savannadashboard.openstack.common import setup as common_setup
+
+requires = common_setup.parse_requirements()
+depend_links = common_setup.parse_dependency_links()
+project = 'savannadashboard'
+
+setuptools.setup(
+    name=project,
+    version=common_setup.get_version(project, '0.2'),
+    description='The Savanna dashboard',
+    author='Mirantis Inc.',
+    author_email='savanna-team@mirantis.com',
+    url='http://savanna.mirantis.com',
+    classifiers=[
+        'Environment :: OpenStack',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    license='Apache Software License',
+    cmdclass=common_setup.get_cmdclass(),
+    packages=setuptools.find_packages(exclude=['bin']),
+    package_data={'savannadashboard': [
+        'hadoop/templates/hadoop/*.html',
+    ]},
+    install_requires=requires,
+    dependency_links=depend_links,
+    setup_requires=['setuptools-git>=0.4'],
+    include_package_data=True,
+    test_suite='nose.collector',
+    py_modules=[],
+)
