e63c883529a85f87491660c0f42f9121f752adcd
Sergey Lukjanov <slukjanov@mirantis.com>
2013-08-23 17:16:29 +0400

    Migrate to pbr
    Change-Id: I58f8d9138c87386af8b656ba64cb474ce1d1bda7

---
+++ README.rst
@@ -0,0 +1,23 @@
+OpenStack Dashboard plugin for Savanna project
+==============================================
+
+Savanna project: https://github.com/stackforge/savanna
+
+Savanna at wiki.openstack.org: https://wiki.openstack.org/wiki/Savanna
+
+Launchpad project: https://launchpad.net/savanna
+
+Savanna docs site: https://savanna.readthedocs.org/en/latest/index.html
+
+Roadmap: https://wiki.openstack.org/wiki/Savanna/Roadmap
+
+Quickstart guide: https://savanna.readthedocs.org/en/latest/devref/quickstart.html
+
+How to participate: https://savanna.readthedocs.org/en/latest/devref/how_to_participate.html
+
+
+License
+-------
+Copyright (c) 2013 Mirantis Inc.
+
+Apache License Version 2.0 http://www.apache.org/licenses/LICENSE-2.0
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
-# Copyright (c) 2013 Mirantis Inc.
+#!/usr/bin/env python
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,41 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
 
-from savannadashboard.openstack.common import setup as common_setup
-
-requires = common_setup.parse_requirements()
-depend_links = common_setup.parse_dependency_links()
-project = 'savanna-dashboard'
-
 setuptools.setup(
-    name=project,
-    version=common_setup.get_version(project, '0.2'),
-    description='The Savanna dashboard',
-    author='OpenStack',
-    author_email='openstack-dev@lists.openstack.org',
-    url='https://savanna.readthedocs.org',
-    classifiers=[
-        'Environment :: OpenStack',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-    ],
-    license='Apache Software License',
-    cmdclass=common_setup.get_cmdclass(),
-    packages=setuptools.find_packages(exclude=['bin']),
-    package_data={'savannadashboard': [
-        '*.html',
-    ]},
-    install_requires=requires,
-    dependency_links=depend_links,
-    setup_requires=['setuptools-git>=0.4'],
-    include_package_data=True,
-    test_suite='nose.collector',
-    py_modules=[],
-)
+    setup_requires=['pbr>=0.5.21,<1.0'],
+    pbr=True)
