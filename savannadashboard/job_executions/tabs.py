d1c331638c43d10072c9cf954312b6d36767f13f
Chad Roberts <croberts@redhat.com>
2014-01-30 09:04:01 -0500

    Adding object names to job-execution details page
    The user-assigned name for input/output, cluster and
    job is now displayed in parens following the object id
    on the job-execution details page.
    
    Partial-Bug: #1269572
    
    Change-Id: Ic1302eb9a7178bf7b9add6c6b67eb34d5b6484fa

--- savannadashboard/job_executions/tabs.py
+++ savannadashboard/job_executions/tabs.py
@@ -33,7 +33,39 @@ class GeneralTab(tabs.Tab):
         job_execution_id = self.tab_group.kwargs['job_execution_id']
         savanna = savannaclient(request)
         job_execution = savanna.job_executions.get(job_execution_id)
-        return {"job_execution": job_execution}
+        object_names = self.get_object_names(job_execution, savanna)
+
+        return {"job_execution": job_execution,
+                "object_names": object_names}
+
+    def get_object_names(self, job_ex, s_client):
+        object_names = {}
+        obj_names_map = {'input_name': {'obj': 'data_sources',
+                                        'obj_id': job_ex.input_id},
+                         'output_name': {'obj': 'data_sources',
+                                         'obj_id': job_ex.output_id},
+                         'cluster_name': {'obj': 'clusters',
+                                          'obj_id': job_ex.cluster_id},
+                         'job_name': {'obj': 'jobs',
+                                      'obj_id': job_ex.job_id}}
+        for item in obj_names_map:
+            object_names[item] = (
+                self.get_object_name(obj_names_map[item]['obj_id'],
+                                     obj_names_map[item]['obj'],
+                                     s_client))
+
+        return object_names
+
+    def get_object_name(self, obj_id, savanna_obj, s_client):
+        object_name = None
+        try:
+            s_obj = getattr(s_client, savanna_obj)
+            obj = s_obj.get(obj_id)
+            object_name = obj.name
+        except Exception as e:
+            LOG.warn("Unable to get name for %s with object_id %s (%s)" %
+                     (savanna_obj, obj_id, str(e)))
+        return object_name
 
 
 class JobExecutionDetailsTabs(tabs.TabGroup):
