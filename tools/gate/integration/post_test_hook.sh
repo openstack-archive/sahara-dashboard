017beb40f9f04e972509b5b763c5c8025ab78533
Vitaly Gridnev <vgridnev@mirantis.com>
2015-12-09 15:57:47 +0300

    Add intial test hooks for integration tests
    Change-Id: Iad7898f1823130beccc10bab294b959bafb72958

---
+++ tools/gate/integration/post_test_hook.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script will be executed inside post_test_hook function in devstack gate
+
+set -ex
+source commons $@
---
+++ tools/gate/integration/pre_test_hook.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script will be executed inside pre_test_hook function in devstack gate
+
+set -ex
+source commons $@
