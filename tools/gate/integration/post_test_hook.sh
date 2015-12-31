6eafe0e62080e473f9a2df003bb87613f1c7ed83
Timur Sufiev <tsufiev@mirantis.com>
2015-12-31 13:06:16 +0300

    Run actual integration tests in post-install hook
    Change-Id: I8baa8783d8bcdf36a45da21b5e68bdea163c7a3a

--- tools/gate/integration/post_test_hook.sh
+++ tools/gate/integration/post_test_hook.sh
@@ -4,3 +4,11 @@
 
 set -ex
 source commons $@
+
+cd /opt/stack/new/sahara-dashboard
+sudo -H -u stack tox -e py27integration
+retval=$?
+if [ -d ${SAHARA_DASHBOARD_SCREENSHOTS_DIR}/ ]; then
+  cp -r ${SAHARA_DASHBOARD_SCREENSHOTS_DIR}/ /home/jenkins/workspace/gate-sahara-dashboard-dsvm-integration/
+fi
+exit $retval
