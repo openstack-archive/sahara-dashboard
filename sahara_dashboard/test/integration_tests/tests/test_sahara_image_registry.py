7d3f48c0ce5b39ef6677a3ae329d618e1673dc05
Chad Roberts <croberts@redhat.com>
2016-02-18 13:59:50 -0500

    Fixing up integration tests after UI reorganization
    This patch reorganizes the integration tests so that the
    pages are in-line with the new UI layout.
    
    Change-Id: Icf3fb352187698f544dd7234d66df1f9c5c65a16
    Partial-Implements: bp reduce-number-of-panels

--- sahara_dashboard/test/integration_tests/tests/test_sahara_image_registry.py
+++ sahara_dashboard/test/integration_tests/tests/test_sahara_image_registry.py
@@ -28,7 +28,8 @@ class TestSaharaImageRegistry(helpers.TestCase):
 
     def test_image_register_unregister(self):
         """Test the image registration in Sahara."""
-        image_reg_pg = self.home_pg.go_to_dataprocessing_imageregistrypage()
+        image_reg_pg = \
+            self.home_pg.go_to_dataprocessing_clusters_dataimageregistrypage()
         image_reg_pg.register_image(IMAGE_NAME, self.CONFIG.scenario.ssh_user,
                                     "Test description")
         image_reg_pg.wait_until_image_registered(IMAGE_NAME)
