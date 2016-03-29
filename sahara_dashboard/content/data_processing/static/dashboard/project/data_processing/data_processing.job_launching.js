ab99844e0b0dc7855da32a464758d9bebb6844e1
Michael Ionkin <mionkin@mirantis.com>
2016-03-29 17:58:49 +0300

    Fix job launching with cluster in any state
    Now we can launch job only on clusters that in Active or Error state.
    * added 'in {state} state' phrase to cluster selection field for
      clusters that in the state that differs from 'Active' state.
    * added warning message for the choice of the cluster in the state
      that differs from 'Active' state.
    * added 'No clusters available' message if there are no suitable
      clusters for a job launching.
    
    Change-Id: I949b81290f94370eb0709f0a04e4d2ce39855323
    bp: cluster-choice-for-jobs
    closes-bug: 1430232

---
+++ sahara_dashboard/content/data_processing/static/dashboard/project/data_processing/data_processing.job_launching.js
@@ -0,0 +1,21 @@
+horizon.job_launching_warning = {
+
+    show_warning: function(cl_name_status_map, status_message_map) {
+        var cl_choice_elem = $('.cluster_choice').parent();
+        var current_cl_name = $('.cluster_choice option:selected').text();
+        var current_cl_status = cl_name_status_map[current_cl_name];
+        var warning_msg = status_message_map[current_cl_status];
+        var warning_elem = "<div class=\"not_active_state_warning alert alert-dismissable alert-warning\">" +
+                            "<h4>" + gettext("Warning!") + "</h4>" +
+                            "<p>" + warning_msg + "</p>" +
+                            "</div>";
+
+        if ($.isEmptyObject(cl_name_status_map) || current_cl_status === 'Active') {
+            $('.not_active_state_warning').remove();
+        } else {
+            cl_choice_elem.append(warning_elem);
+            $('.not_active_state_warning').css('margin-top', '5px');
+        }
+    }
+
+}
