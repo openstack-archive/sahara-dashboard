f1933d84a8a9d0abd3d0330d8114522a146138ed
Vitaly Gridnev <vgridnev@mirantis.com>
2016-06-11 20:04:35 +0000

    fix integration tests
    hot fix is taken from https://review.openstack.org/#/c/327883/
    
    Change-Id: I952f0f07cca202cdc5dd84f72efe6ecce6c477eb

--- tools/gate/integration/post_test_hook.sh
+++ tools/gate/integration/post_test_hook.sh
@@ -7,6 +7,12 @@ source commons $@
 
 set +e
 cd /opt/stack/new/sahara-dashboard
+
+sudo wget -q -O firefox.deb https://sourceforge.net/projects/ubuntuzilla/files/mozilla/apt/pool/main/f/firefox-mozilla-build/firefox-mozilla-build_46.0.1-0ubuntu1_amd64.deb/download
+sudo apt-get -y purge firefox
+sudo dpkg -i firefox.deb
+sudo rm firefox.deb
+
 sudo -H -u stack tox -e py27integration
 retval=$?
 set -e
