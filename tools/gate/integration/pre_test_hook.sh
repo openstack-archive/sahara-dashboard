f4de8b16450a1088cb112e0d91d7717c1bc72b0a
Mikhail Lelyakin <mlelyakin@mirantis.com>
2016-08-30 15:27:06 +0300

    Fix integration tests
    Images table in openstack dashboard renders
    by Angular now. There are no integration tests
    in Horizon for test this table, but there is
    possibility to configure table to old-style.
    In this patch I ovveride the configurations
    provided by the panels and force a 'legacy' setting.
    Based upon change: https://review.openstack.org/#/c/349020/
    
    Change-Id: I3382bed3ff516a14d5b72ed3a7bedc409b0de8eb

--- tools/gate/integration/pre_test_hook.sh
+++ tools/gate/integration/pre_test_hook.sh
@@ -7,3 +7,13 @@ source commons $@
 
 cd /opt/stack/new/horizon/openstack_dashboard/local/local_settings.d
 mv _20_integration_tests_scaffolds.py.example _20_integration_tests_scaffolds.py
+
+mv _2010_integration_tests_deprecated.py.example _2010_integration_tests_deprecated.py
+cat > /opt/stack/new/horizon/openstack_dashboard/test/integration_tests/local-horizon.conf <<EOF
+[image]
+panel_type=legacy
+
+[flavors]
+panel_type=legacy
+EOF
+
