18921beb2ed111b97d1eaca55c3c778c7d6b3890
Luigi Toscano <ltoscano@redhat.com>
2020-04-15 17:59:11 +0200

    Test fixes: unittest.mock, better l-c job, pytest dep
    Now only python 3 is supported, which means unittest.mock
    can be used instead of the external mock module.
    This change can be done thanks to the Horizon
    change I2de669d8e89b8daeb7ee5405ffab35af6307c40b
    
    Switch to openstack-lower-constraints-jobs-horizon
    which pulls in the last horizon. The lower-constraint
    version of horizon should be bumped when a new
    version (post-18.2.0) is released.
    
    Finally, add pytest to the set of test requirements
    to prevent an missing import error (even through
    the sahara scenario tests still need to be ported
    to pytest).
    
    Change-Id: I2c61e9d88b2d05c9f0b68066d7bb15b43cdf083f

--- sahara_dashboard/content/data_processing/jobs/data_sources/tests.py
+++ sahara_dashboard/content/data_processing/jobs/data_sources/tests.py
@@ -10,8 +10,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from unittest import mock
+
 from django.urls import reverse
-import mock
 
 from sahara_dashboard import api
 from sahara_dashboard.test import helpers as test
